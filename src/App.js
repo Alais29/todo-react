67343f8c40c6dd1128c05090e2388ab639a8028d
Alfonsina <alfonsina.l@gmail.com>
2020-05-02 17:52:49 -0400

    Added node-sass and modified style files

--- src/App.js
+++ src/App.js
@@ -1,47 +1,49 @@
-import React, {Fragment} from 'react';
-import TodoItem from "./components/TodoItem/TodoItem.component"
-import todosData from "./todosData"
-import './App.css';
+import React, { Fragment } from "react";
+import TodoItem from "./components/TodoItem/TodoItem.component";
+import todosData from "./todosData";
+import "./App.css";
 
 class App extends React.Component {
   constructor() {
-      super();
-      this.state = {
-          todos: todosData,
-      };
-      this.handleChange = this.handleChange.bind(this);
+    super();
+    this.state = {
+      todos: todosData,
+    };
+    this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(id) {
-      this.setState(prevState => {
-          const updatedTodos = prevState.todos.map(todo => {
-              if(todo.id === id) {
-                  return {
-                    ...todo,
-                    completed: !todo.completed
-                  }
-              }
-              return todo;
-          })
+    this.setState((prevState) => {
+      const updatedTodos = prevState.todos.map((todo) => {
+        if (todo.id === id) {
           return {
-              todos: updatedTodos,
-          }
-      })
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      });
+      return {
+        todos: updatedTodos,
+      };
+    });
   }
-  
+
   render() {
-      return (
-          <Fragment>
-              <h1>Todos List</h1>
-              <div className="list">
-                  {
-                      this.state.todos.map(item => (
-                          <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>
-                      ))
-                  }
-              </div>
-          </Fragment>
-      )
+    return (
+      <Fragment>
+        <h1>Todos List</h1>
+        <div className="list">
+          {this.state.todos.map((item) => (
+            <TodoItem
+              key={item.id}
+              item={item}
+              handleChange={this.handleChange}
+            />
+          ))}
+        </div>
+      </Fragment>
+    );
   }
 }
 
--- src/components/TodoItem/TodoItem.component.jsx
+++ src/components/TodoItem/TodoItem.component.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import './TodoItem.styles.scss'
+
 const TodoItem = (props) => (
   <div className={props.item.completed ? 'item item-done' : 'item'}>
     <input type="checkbox" id={props.item.id} checked={props.item.completed} onChange={() => props.handleChange(props.item.id)} />
