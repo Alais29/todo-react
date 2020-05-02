d95da07d5f0e782b7b33a74d313795c475cbe180
Alfonsina <alfonsina.l@gmail.com>
2020-05-02 17:35:40 -0400

    First commir

---
+++ src/components/TodoItem/TodoItem.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const TodoItem = (props) => (
+  <div className={props.item.completed ? 'item item-done' : 'item'}>
+    <input type="checkbox" id={props.item.id} checked={props.item.completed} onChange={() => props.handleChange(props.item.id)} />
+    <label htmlFor={props.item.id} className="ml-1">
+      {props.item.text}
+      <span className={props.item.completed ? 'line line-through' : 'line'}></span>
+    </label>
+  </div>
+);
+
+export default TodoItem;
